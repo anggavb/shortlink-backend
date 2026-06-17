a6860715536f25c3b48c6b87c17a79847915b6a8
anggavb <anggavb8@gmail.com>
2026-06-18 00:37:49 +0700

    refactor(auth): update SaveToken method to use UserWithToken struct and remove unused functions

--- internal/repository/auth_cache.repository.go
+++ internal/repository/auth_cache.repository.go
@@ -31,26 +31,18 @@ func (r *AuthCacheRepository) SaveToken(ctx context.Context, tokenHash string, u
 		return errors.New("token already expired")
 	}
 
-	userJSON, err := json.Marshal(user)
+	userWithToken := model.UserWithToken{
+		User:  user,
+		Token: tokenHash,
+	}
+
+	userJSON, err := json.Marshal(userWithToken)
 	if err != nil {
 		return err
 	}
 
 	return r.rdb.Set(ctx, r.tokenKey(user.Id, tokenHash), userJSON, ttl).Err()
 }
-
-// func (r *AuthCacheRepository) IsTokenActive(ctx context.Context, tokenHash string, userID int) (bool, error) {
-// 	exists, err := r.rdb.Exists(ctx, r.tokenKey(userID, tokenHash)).Result()
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	return exists == 1, nil
-// }
-
-// func (r *AuthCacheRepository) DeleteToken(ctx context.Context, tokenHash string, userID int) error {
-// 	return r.rdb.Del(ctx, r.tokenKey(userID, tokenHash)).Err()
-// }
 
 func (r *AuthCacheRepository) tokenKey(userID int, tokenHash string) string {
 	key := fmt.Sprintf("auth:token~%d:%s", userID, tokenHash)
@@ -60,41 +52,3 @@ func (r *AuthCacheRepository) tokenKey(userID int, tokenHash string) string {
 
 	return r.prefix + ":" + key
 }
-
-// func (r *AuthCacheRepository) StoreTokenForgotPassword(
-// 	ctx context.Context,
-// 	token string,
-// 	userID int,
-// 	expiresAt time.Time,
-// ) error {
-
-// 	ttl := time.Until(expiresAt)
-// 	if ttl <= 0 {
-// 		return errors.New("token already expired")
-// 	}
-
-// 	key := fmt.Sprintf("%s:auth:reset-password:%s", r.prefix, token)
-// 	log.Println(key)
-// 	err := r.rdb.Set(ctx, key, userID, ttl).Err()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func (r *AuthCacheRepository) IsForgotPasswordKeyActive(ctx context.Context, key string) (bool, error) {
-// 	exists, err := r.rdb.Exists(ctx, key).Result()
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	return exists == 1, nil
-// }
-
-// func (r *AuthCacheRepository) GetValueAndDelete(ctx context.Context, key string) (string, error) {
-// 	val, err := r.rdb.GetDel(ctx, key).Result()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return val, nil
-// }
