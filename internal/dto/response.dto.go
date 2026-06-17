9ee5689956eb02d051cd1ee6463814bfdfcc0076
anggavb <anggavb8@gmail.com>
2026-06-18 04:31:29 +0700

    refactor(response): rename is_success to success and update data field to results

--- internal/dto/response.dto.go
+++ internal/dto/response.dto.go
@@ -1,9 +1,9 @@
 package dto
 
 type Response struct {
+	Success bool   `json:"success"`
 	Message string `json:"message"`
-	Success bool   `json:"is_success"`
-	Data    any    `json:"data,omitempty"`
+	Results any    `json:"results,omitempty"`
 	Error   string `json:"error,omitempty"`
 	Errors  any    `json:"errors,omitempty"`
 }
